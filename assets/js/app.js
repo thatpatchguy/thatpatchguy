74602c51270c7c0c896dfebcfe91449d1f126e04
Dylan Patch <dpatch15@gmail.com>
2021-02-03 15:24:00 -0700

    chord route

--- assets/js/app.js
+++ assets/js/app.js
@@ -227,7 +227,7 @@ function song_selected(){
         the_chords = chord_data[1][0];
         console.log(the_chords);
         console.log(chord_data);
-        for (const property in the_chords.all_chords){
+        for (const property in the_chords.all_chords[0]){
             html_stuff += `<tr><td>${property}</td><td>${the_chords[property]}</td></tr>`;
         }
         html_stuff += `</table>`;
